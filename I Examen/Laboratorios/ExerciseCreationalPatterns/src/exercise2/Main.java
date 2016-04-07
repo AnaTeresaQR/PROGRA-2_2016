053c6e5b0676ed5507353c9320e587f7128a813a
AnaTeresaQR <ana.quesada@ucrso.info>
2016-04-07 12:53:58 -0600

    no funciona el builder

--- I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise2/Main.java
+++ I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise2/Main.java
@@ -3,6 +3,8 @@ package exercise2;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -11,6 +13,23 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            AbstractBuilder ab = new ConcreteBuilder();
+            Director director = new Director();
+
+            BossProyect boss = new BossProyect("Tere", "Quesada", "207410170");
+
+            Date d1 = createDate(2010, 7, 29);
+
+            ArrayList<Date> array = new ArrayList<>();
+            array.add(d1);
+
+            Proyect p1 = director.createProyect(ab, "Proyecto 1", boss, 2, array);
+        } catch (ProyectException ex) {
+            System.out.println("" + ex.getMessage());
+        }
+
     }
 
     public static Date createDate(int year, int month, int date) {
