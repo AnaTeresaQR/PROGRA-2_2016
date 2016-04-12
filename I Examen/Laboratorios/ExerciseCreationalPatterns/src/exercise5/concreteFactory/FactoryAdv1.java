8711162f45583b7080d047fcc2932ed77e57cac3
AnaTeresaQR <ana.quesada@ucrso.info>
2016-04-11 20:47:26 -0600

    Se termina el ejercicio número 5 patrones de Creación

--- I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise5/concreteFactory/FactoryAdv1.java
+++ I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise5/concreteFactory/FactoryAdv1.java
@@ -2,7 +2,9 @@ package exercise5.concreteFactory;
 
 import exercise5.abstractFactory.NewspaperAdvertising_Factory;
 import exercise5.concreteProduct.AlDia.Adv_AlDia1;
+import exercise5.concreteProduct.LaNacion.Adv_LaNacion1;
 import exercise5.product.Adv_AlDia;
+import exercise5.product.Adv_LaNacion;
 
 /**
  *
@@ -13,6 +15,10 @@ public class FactoryAdv1 implements NewspaperAdvertising_Factory {
     @Override
     public Adv_AlDia createAlDiaNewspaper() {
         return new Adv_AlDia1();
+    }
+
+    public Adv_LaNacion createLaNacionNewspaper() {
+        return new Adv_LaNacion1();
     }
 
 }
--- I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise5/concreteFactory/FactoryAdv1_4.java
+++ I Examen/Laboratorios/ExerciseCreationalPatterns/src/exercise5/concreteFactory/FactoryAdv1_4.java
@@ -2,7 +2,9 @@ package exercise5.concreteFactory;
 
 import exercise5.abstractFactory.NewspaperAdvertising_Factory;
 import exercise5.concreteProduct.AlDia.Adv_AlDia1_4;
+import exercise5.concreteProduct.LaTeja.Adv_LaTeja1_4;
 import exercise5.product.Adv_AlDia;
+import exercise5.product.Adv_LaTeja;
 
 /**
  *
@@ -13,5 +15,9 @@ public class FactoryAdv1_4 implements NewspaperAdvertising_Factory {
     @Override
     public Adv_AlDia createAlDiaNewspaper() {
         return new Adv_AlDia1_4();
+    }
+
+    public Adv_LaTeja createLaTejaNewspaper() {
+        return new Adv_LaTeja1_4();
     }
 }
